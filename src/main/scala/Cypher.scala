56465a95907dec83f5884c6976d1559989ab4185
Samuel Coleman <samuelwjcoleman@gmail.com>
2019-01-11 15:51:33 +0000

    Friday

--- src/main/scala/Cypher.scala
+++ src/main/scala/Cypher.scala
@@ -1,9 +1,8 @@
 object Cypher extends App{
-
   def cypher(input:String):String={
     val alphabet = ('a' to 'z').toArray
     var output=""
-    for(i <- 0 until input.length)output += alphabet(25-alphabet.indexOf(input(i))).toString
+    for(i <- 0 until input.length)output += alphabet(26-alphabet.indexOf(input(i))).toString
     output
   }
   println("Type the word you want to code")
